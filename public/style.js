4b1d14858604a2694261d972d4f797173bbf3cf5
Paul Borisade <108955782+Younglad-Paul@users.noreply.github.com>
2023-08-25 00:09:52 +0100

    recent

--- public/style.js
+++ public/style.js
@@ -5,3 +5,15 @@ function DownloadCV() {
     cv.download = "img/BORISADE+DAYO+PAUL+CV+1+(1).pdf";
     cv.dispatchEvent( new MouseEvent('click'))
 }
+
+// const nav = document.querySelector("nav")
+
+// window.addEventListener("scroll", () => {
+//         if (scrollY > 50) {
+//             nav.style.background = '#016306';
+//         }
+//         else if (scrollY < 8000) {
+//             Tmenu.style.background = '#12be1b';
+//         }
+//     }
+// )
