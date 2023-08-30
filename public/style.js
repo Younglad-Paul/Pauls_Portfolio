4704c786f1f22428f7886cd6d57690b5a8eb8ba7
Paul Borisade <108955782+Younglad-Paul@users.noreply.github.com>
2023-08-31 00:45:09 +0100

    development

--- public/style.js
+++ public/style.js
@@ -1,19 +1,6 @@
 function DownloadCV() {
-    // document.location = "img/BORISADE DAYO PAUL CV 1 (1).pdf";
-    var cv = document.createElement('a');
-    cv.href = "images/BORISADE+DAYO+PAUL+CV+1+(1).pdf";
-    cv.download = "img/BORISADE+DAYO+PAUL+CV+1+(1).pdf";
-    cv.dispatchEvent( new MouseEvent('click'))
+    const cvLink = document.createElement('a');
+    cvLink.href = "images/BORISADE+DAYO+PAUL+CV+1+(1).pdf";
+    cvLink.download = "BORISADE_DAYO_PAUL_CV.pdf";
+    cvLink.click();
 }
-
-// const nav = document.querySelector("nav")
-
-// window.addEventListener("scroll", () => {
-//         if (scrollY > 50) {
-//             nav.style.background = '#016306';
-//         }
-//         else if (scrollY < 8000) {
-//             Tmenu.style.background = '#12be1b';
-//         }
-//     }
-// )
