7d8a129db8a41f58eec7b05fb1f483eb4da0ba17
Paul Borisade <108955782+Younglad-Paul@users.noreply.github.com>
2023-08-31 01:27:36 +0100

    revised at latest

--- public/style.js
+++ public/style.js
@@ -1,6 +1,6 @@
 function DownloadCV() {
     const cvLink = document.createElement('a');
-    cvLink.href = "images/BORISADE+DAYO+PAUL+CV+1+(1).pdf";
+    cvLink.href = "images/BORISADE DAYO PAUL CV 1.pdf";
     cvLink.download = "BORISADE_DAYO_PAUL_CV.pdf";
     cvLink.click();
 }
